286f2d6535254af8e59a6bc938b5d30388c165b1
ethan-air <ethanlevien@gmail.com>
2026-01-28 09:07:27 -0500

    addded copilot instructions

--- paper-figures/examples.jl
+++ paper-figures/examples.jl
@@ -1,35 +1,40 @@
 
+
+## OU Model with Division Noise and failing condition I
 function M0()
     λ(x) = 1.0 + x[1]
     α = 0.5
     σY = 0.01
     σX = 0.01
-    D = 0.01
+    D = 0.05
+    γ = 1. 
+
+    # 
 
     function β(x, z, t)
         y0 = z[1]- z[2]
         μ = log(2) - α * y0
         A = exp(-((z[1] - μ)^2) / (2 * σY^2))/(2 * π * σY^2)^(1/2)
         B = (1 - erf((z[1] - μ) / (sqrt(2) * σY)))
-        return  λ(x)*A/B
+        return  A/B
     end
 
     # symmetric division 
     h(z, x) = ([z[1] - log(2) .+ rand(Normal(0,0.1)),0],x .+ rand(Normal(0,σX),3))
 
     # OU process dynamics of x
-    L(x, t, dt) =   -x*dt .+ sqrt(dt*D) .*rand(Normal(0,1),3)
-
+    L(x, t, dt) =   -γ*x*dt .+ sqrt(dt*D) .*rand(Normal(0,1),3)
 
     return λ, β, h, L
 end
 
+## OU Model with no Division Noise
 function M1()
     λ(x) = 1.0 + x[1]
     α = 0.5
     σY = 0.01
-    D = 0.01
-    σX = 0.01
+    D = 0.05
+    γ = 1. 
 
     function β(x, z, t)
         y0 = z[1]- z[2]
@@ -40,10 +45,10 @@ function M1()
     end
 
     # symmetric division 
-    h(z, x) = ([z[1] - log(2)  .+ rand(Normal(0,0.1)),0],x .+ rand(Normal(0,σX),3))
+    h(z, x) = ([z[1] - log(2),0],x)
 
     # OU process dynamics of x
-    L(x, t, dt) = -0.5 .*x*dt .+ sqrt(dt*D) .*rand(Normal(0,1),3)
+    L(x, t, dt) = -γ*x*dt .+ sqrt(dt*D) .*rand(Normal(0,1),3)
 
     return λ, β, h, L
 end
@@ -54,6 +59,7 @@ function M2()
     α = 0.5
     σY = 0.01
     σX = 0.01
+    D = 0.05
 
     function β(x, z, t)
         y0 = z[1]- z[2]
@@ -66,7 +72,7 @@ function M2()
     # symmetric division 
     h(z, x) = ([z[1] - log(2)  .+ rand(Normal(0,0.1)),0],x .+ rand(Normal(0,σX),3))
 
-    # OU process dynamics of x
+    # constant x in cell-cycle
     L(x, t, dt) = 0.0
 
     model = GrowthModel(λ, β, h, L)
