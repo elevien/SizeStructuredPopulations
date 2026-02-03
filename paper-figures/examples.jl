0d62b3ee780e2e3bde3eed943e66791963270f4f
ethan-air <ethanlevien@gmail.com>
2026-02-03 16:25:43 -0500

    Code for revised version of paper

--- paper-figures/examples.jl
+++ paper-figures/examples.jl
@@ -1,14 +1,15 @@
 
 
+
 ## OU Model with Division Noise and failing condition I
 function M0()
     λ(x) = 1.0 + x[1]
-    α = 0.5
-    σY = 0.01
-    σX = 0.01
-    D = 0.05
-    γ = 1. 
 
+    α = 0.5
+    σY = 0.05
+    D = 0.025 * log(2)  # D = 0.1 * γ² * ln(2) / 2 = 0.1 * 1² * ln(2) / 2
+    γ = 1. 
+    σX = sqrt(0.0001)
     # 
 
     function β(x, z, t)
@@ -19,11 +20,16 @@ function M0()
         return  A/B
     end
 
-    # symmetric division 
-    h(z, x) = ([z[1] - log(2) .+ rand(Normal(0,0.1)),0],x .+ rand(Normal(0,σX),3))
+    # symmetric division with Gamma noise (same mean=0, variance=σX²)
+    function gamma_noise(σ)
+        # Gamma(1, σ) has mean=σ, variance=σ²
+        # Shift by -σ to get mean=0, variance=σ²
+        return rand(Gamma(1, σ)) - σ *0.9
+    end
+    h(z, x) = ([z[1] - log(2) + rand(Normal(0,0.05)),0],x)
 
-    # OU process dynamics of x
-    L(x, t, dt) =   -γ*x*dt .+ sqrt(dt*D) .*rand(Normal(0,1),3)
+    # OU process dynamics of x (exact distribution)
+    L(x, t, dt) = x .* (exp(-γ*dt) .-1) .+ sqrt(D * (1 - exp(-2*γ*dt))/(2*γ)) .* rand(Normal(0,1),3)
 
     return λ, β, h, L
 end
@@ -32,9 +38,9 @@ end
 function M1()
     λ(x) = 1.0 + x[1]
     α = 0.5
-    σY = 0.01
-    D = 0.05
-    γ = 1. 
+    σY = 0.05
+    γ = 1.
+    D = 0.025 * log(2)  # D = 0.1 * γ² * ln(2) / 2 = 0.1 * 1² * ln(2) / 2
 
     function β(x, z, t)
         y0 = z[1]- z[2]
@@ -45,10 +51,10 @@ function M1()
     end
 
     # symmetric division 
-    h(z, x) = ([z[1] - log(2),0],x)
+    h(z, x) = ([z[1] - log(2) + rand(Normal(0,0.05)),0],x)
 
-    # OU process dynamics of x
-    L(x, t, dt) = -γ*x*dt .+ sqrt(dt*D) .*rand(Normal(0,1),3)
+    # OU process dynamics of x (exact distribution)
+    L(x, t, dt) = x .* (exp(-γ*dt) -1)  .+ sqrt(D * (1 - exp(-2*γ*dt))/(2*γ)) .* rand(Normal(0,1),3)
 
     return λ, β, h, L
 end
@@ -57,9 +63,9 @@ end
 function M2()
     λ(x) = 1.0 + x[1]
     α = 0.5
-    σY = 0.01
-    σX = 0.01
-    D = 0.05
+    σY = 0.05
+    σX = sqrt(0.005)  # σX = sqrt(v_DN) = sqrt(0.1)
+
 
     function β(x, z, t)
         y0 = z[1]- z[2]
@@ -69,8 +75,13 @@ function M2()
         return  λ(x)*A/B
     end
 
-    # symmetric division 
-    h(z, x) = ([z[1] - log(2)  .+ rand(Normal(0,0.1)),0],x .+ rand(Normal(0,σX),3))
+    # symmetric division with Gamma noise (same mean=0, variance=σX²)
+    # function gamma_noise(σ)
+    #     # Gamma(1, σ) has mean=σ, variance=σ²
+    #     # Shift by -σ to get mean=0, variance=σ²
+    #     return rand(Gamma(1, σ)) - σ *0.9
+    # end
+    h(z, x) = ([z[1] - log(2) + rand(Normal(0,0.05)),0],rand(Normal(0,σX),3))
 
     # constant x in cell-cycle
     L(x, t, dt) = 0.0
